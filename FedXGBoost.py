a784fbfe1514f2fc1f87446ad31ea3aba22ea95a
KhanhLn <khanhln2907@gmail.com>
2022-04-14 15:40:10 +0200

    Tested Send/Receiv of MPI

--- FedXGBoost.py
+++ FedXGBoost.py
@@ -1,7 +1,15 @@
+from numpy import concatenate
 from VerticalXGBoost import *
 from SSCalculation import *
 import matplotlib.pyplot as plt
 import logging
+
+class PARTY_ID:
+    ACTIVE_PARTY = 1
+
+
+class MSG_ID:
+    MASKED_GH = 99
 
 class VerticalFedXGBoostTree(VerticalXGBoostTree):
     def __init__(self, rank, lossfunc, splitclass, _lambda, _gamma, _epsilon, _maxdepth, clientNum):
@@ -35,7 +43,7 @@ class VerticalFedXGBoostTree(VerticalXGBoostTree):
 
 
         # Compute the gradients
-        if self.rank == 1: # Calculate gradients on the node who have labels.
+        if self.rank == PARTY_ID.ACTIVE_PARTY: # Calculate gradients on the node who have labels.
             y, y_pred = self._split(y_and_pred)
             G = self.loss.gradient(y, y_pred)
             H = self.loss.hess(y, y_pred)
@@ -43,6 +51,20 @@ class VerticalFedXGBoostTree(VerticalXGBoostTree):
             logger.debug("G {}".format(' '.join(map(str, G))))
             logger.debug("H {}".format(' '.join(map(str, H))))
 
+            gh = np.concatenate((G, H), axis=1)
+            nprocs = comm.Get_size()
+            for partners in range(2, nprocs):   
+                logger.info("Sending G, H to party %d", partners)         
+                data = comm.send(G, dest = partners, tag = MSG_ID.MASKED_GH)
+            #data = comm.bcast(gh, root = 1)
+            #data = comm.bcast(H, root = 1)
+            
+
+        elif rank != 0: # TODO: change hard coded number
+        #else:
+            data = comm.recv(source=1, tag=MSG_ID.MASKED_GH)
+            logger.info("Received G, H")         
+            # Perform the secure Sharing of the splitting matrix
 
 
 class FedXGBoostClassifier(VerticalXGBoostClassifier):
